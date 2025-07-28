568823c8c070c499404296e4bb2cd7dda1ec9d7a
William Zheng <official.william.zheng@gmail.com>
2025-07-28 03:20:52 -0400

    connected to supabase (tested)

--- val-builder/src/App.jsx
+++ val-builder/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import { supabase } from './client';   // <-- new
 
 function App() {
-
   return (
-        <div>
-            <h1>Home</h1>
-            <NavBar />
-            <main className="p-6">
-                <Outlet />
-            </main>
-        </div>
-  )
+    <div>
+      <h1>Home</h1>
+      <NavBar />
+      <main className="p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- val-builder/src/client.js
+++ val-builder/src/client.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
